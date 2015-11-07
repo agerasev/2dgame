315c629c6f98e47a9b2f5c1c3c63c84407482154
Alexey Gerasev <alexey.gerasev@gmail.com>
2015-11-07 14:30:02 +0600

    c++ graphics

--- source/main.cpp
+++ source/main.cpp
@@ -1,17 +1,30 @@
 #include <media/media.hpp>
-#include <graphics/graphics.h>
+#include <graphics/graphics.hpp>
 
 #include "graphics.hpp"
 
 class GraphicsHandler : public media::Graphics::Handler {
 private:
-	GContext *context;
+	gfx::Context *context;
 	// Graphics graphics;
 	
-	bool inited = false;
-	static void callback(void *data) {
-		static_cast<GraphicsHandler *>(data)->inited = true;
-	}
+	class GFXHandler : public gfx::Handler {
+	public:
+		gfx::Ring *ring;
+		virtual void init() override {
+			ring = new gfx::Ring();
+			ring->setInnerRadius(0.8);
+			ring->setColor(gfx::YELLOW & gfx::ALPHA(0.5));
+			ring->transform(fmat2(100,0,0,100));
+		}
+		virtual void dispose() override {
+			delete ring;
+		}
+		virtual void draw(double dt) override {
+			super->clear();
+			super->drawShape(ring);
+		}
+	} gfx_handler;
 
 public:
 	GraphicsHandler() = default;
@@ -19,32 +32,23 @@ public:
 	
 	virtual void create() override {
 		logMessage("graphics created");
-		context = gCreateContext();
-		gSetInitCallback(context, callback, static_cast<void *>(this));
-		gInit(context);
+		context = new gfx::Context(&gfx_handler);
 	}
 	
 	virtual void destroy() override {
 		logMessage("graphics destroyed");
-		gDispose(context);
-		gDestroyContext(context);
+		delete context;
 	}
 	
 	virtual void resize(int w, int h) override {
 		logMessage("graphics resized %d %d", w, h);
-		gResize(context, w, h);
+		context->resize(w, h);
 	}
 	
 	virtual void draw(double dt) override {
 		// logMessage("frame redrawed dt = %lf", dt);
 		// graphics.draw();
-		float m[4] = {100,0,0,100};
-		if(inited) {
-			gClear(context);
-			gSetColorInt(context, G_RED);
-			gTransform(context, m);
-			gDrawCircle(context);
-		}
+		context->requestDraw(dt);
 	}
 };
 
